a7992d54610613caa2cc2179cc1fdd0ab277f2ad
jum0624 <jum0624@naver.com>
2023-05-04 04:51:17 +0900

    test: AuthServiceTest

---
+++ doormate/src/test/java/com/strongcom/doormate/service/AuthServiceTest.java
@@ -0,0 +1,37 @@
+package com.strongcom.doormate.service;
+
+import com.strongcom.doormate.DoormateApplication;
+import com.strongcom.doormate.security.jwt.payload.request.LoginRequest;
+import com.strongcom.doormate.security.jwt.payload.response.TokenResponse;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DoormateApplication.class)
+public class AuthServiceTest {
+
+    @Autowired
+    private AuthService authService;
+
+    @Test
+    public void testSuccessGenerateAccessTokenAndRefreshToken() throws Exception {
+        LoginRequest request = LoginRequest.builder()
+                .username("admin")
+                .password("admin")
+                .build();
+
+        TokenResponse tokenResponse = authService.generateAccessTokenAndRefreshToken(request);
+        Assert.assertNotNull(tokenResponse);
+    }
+
+    @Test
+    public void testDeleteTokenFromUsername() {
+        authService.deleteTokenFromUsername("strongcom");
+    }
+}
