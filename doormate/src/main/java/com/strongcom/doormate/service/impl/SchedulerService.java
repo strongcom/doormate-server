92ac180e6254ee90292f8d3d657a0d0157aa5c72
jum0624 <jum0624@naver.com>
2023-05-08 00:34:13 +0900

    feat: 알림 리스트 주기적 삭제 기능 추가

---
+++ doormate/src/main/java/com/strongcom/doormate/service/impl/SchedulerService.java
@@ -0,0 +1,19 @@
+package com.strongcom.doormate.service.impl;
+
+import com.strongcom.doormate.repository.AlarmRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+@Service
+@RequiredArgsConstructor
+public class SchedulerService {
+
+    private final AlarmRepository alarmRepository;
+    @Scheduled(cron = "59 59 23 * * *", zone = "Asia/Seoul")
+    public void run() {
+        alarmRepository.deleteAllByNoticeDate(LocalDate.now());
+        System.out.println("금일 미알림 내역 삭제");
+    }
+}
