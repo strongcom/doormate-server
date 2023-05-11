e8a4d4fa051b758599bd255ba3c0b7ec3a9b13c1
jum0624 <jum0624@naver.com>
2023-05-11 16:57:57 +0900

    refactor: AlarmRepository 오늘 알림 조회 로직에 query 수정

--- doormate/src/main/java/com/strongcom/doormate/repository/AlarmRepository.java
+++ doormate/src/main/java/com/strongcom/doormate/repository/AlarmRepository.java
@@ -18,7 +18,7 @@ public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
     List<Alarm> findAllByNoticeDateAndStartTimeLessThanEqualAndEndTimeGreaterThanEqual(LocalDate noticeDate, LocalTime now1, LocalTime now2);
 
-    @Query("select alarm from Alarm alarm where alarm.noticeDate=:today")
+    @Query(value = "select alarm from Alarm alarm where alarm.noticeDate=:today", nativeQuery=true)
     List<Alarm> findAllToday(@Param("today") LocalDate today);
 
     @Transactional
