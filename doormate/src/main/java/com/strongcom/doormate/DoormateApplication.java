0e17b6f0470b8e46a062d4a8d4519aa4026e5848
jum0624 <jum0624@naver.com>
2023-05-11 16:01:02 +0900

    add: 타임존 설절 관련 빈추가

--- doormate/src/main/java/com/strongcom/doormate/DoormateApplication.java
+++ doormate/src/main/java/com/strongcom/doormate/DoormateApplication.java
@@ -6,9 +6,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class DoormateApplication {
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(DoormateApplication.class, args);
