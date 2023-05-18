3d1f2cca4c99ad0030961568cac3c79caccb575b
jum0624 <jum0624@naver.com>
2023-05-18 16:48:05 +0900

    delete: 예외핸들러 삭제

--- doormate/src/main/java/com/strongcom/doormate/handler/GlobalExceptionHandler.java
+++ doormate/src/main/java/com/strongcom/doormate/handler/GlobalExceptionHandler.java
@@ -1,22 +1,22 @@
-package com.strongcom.doormate.handler;
-
-
-import com.strongcom.doormate.exception.NotFoundAlarmException;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-@Slf4j
-@ControllerAdvice
-public class GlobalExceptionHandler {
-
-    // 오늘 울려야할 알림이 없을 경우 응답
-    @ExceptionHandler(NotFoundAlarmException.class)
-    public ResponseEntity<String> handlerNotAlarmException(NotFoundAlarmException e) {
-        log.error(e.getMessage());
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(e.getMessage());
-    }
-}
+//package com.strongcom.doormate.handler;
+//
+//
+//import com.strongcom.doormate.exception.NotFoundAlarmException;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//
+//@Slf4j
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//
+//    // 오늘 울려야할 알림이 없을 경우 응답
+//    @ExceptionHandler(NotFoundAlarmException.class)
+//    public ResponseEntity<String> handlerNotAlarmException(NotFoundAlarmException e) {
+//        log.error(e.getMessage());
+//        return ResponseEntity.status(HttpStatus.OK)
+//                .body(e.getMessage());
+//    }
+//}
