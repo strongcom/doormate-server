7d1e3b4b63df688102f7c3aad711432a5fb15f2c
jum0624 <jum0624@naver.com>
2023-05-22 11:19:02 +0900

    feat: username, nickname Get요청 로직 추가

---
+++ doormate/src/main/java/com/strongcom/doormate/dto/UserInfoRespDto.java
@@ -0,0 +1,16 @@
+package com.strongcom.doormate.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class UserInfoRespDto {
+
+    private String userName;
+    private String nickName;
+
+}
